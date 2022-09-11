ef9417d97d692554b07176cd10bb2a2fc3d01ad3
labijay <yjinadu@immomio.de>
2022-09-11 13:39:08 +0200

    trying prettier

--- cypress/e2e/login/login.spec.js
+++ cypress/e2e/login/login.spec.js
@@ -2,40 +2,39 @@ import loginPage from '../../support/page-objects/login/loginPage';
 import securePage from '../../support/page-objects/secure/securePage';
 const fixture = 'login/login';
 describe('Login functionality should work correctly', function () {
-	beforeEach(() => {
-		cy.fixture(fixture).then(function ({ correctCredentials, wrongCredentials }) {
-			this.correctUsername = correctCredentials.username;
-			this.correctPassword = correctCredentials.password;
-			this.wrongUsername = wrongCredentials.username;
-			this.wrongPassword = wrongCredentials.password;
-		});
-		cy.visit(`${Cypress.env('fe_base_url') + loginPage.getRoute()} `);
-	});
-	it('User should be able to login with correct credential', function () {
-		loginPage.tryLogin(this.correctUsername, this.correctPassword);
-		cy.url().should('eq', `${Cypress.env('fe_base_url') + securePage.getRoute()}`);
-		cy.contains(
-			securePage.getLoggedInbanner(),
-			securePage.getTexts().loggedInBannerText
-		).should('exist');
-	});
+    beforeEach(() => {
+        cy.fixture(fixture).then(function ({ correctCredentials, wrongCredentials }) {
+            this.correctUsername = correctCredentials.username;
+            this.correctPassword = correctCredentials.password;
+            this.wrongUsername = wrongCredentials.username;
+            this.wrongPassword = wrongCredentials.password;
+        });
+        cy.visit(`${Cypress.env('fe_base_url') + loginPage.getRoute()} `);
+    });
+    it('User should be able to login with correct credential', function () {
+        loginPage.tryLogin(this.correctUsername, this.correctPassword);
+        cy.url().should('eq', `${Cypress.env('fe_base_url') + securePage.getRoute()}`);
+        cy.contains(
+            securePage.getLoggedInbanner(),
+            securePage.getTexts().loggedInBannerText
+        ).should('exist');
+    });
 
-	it('User should be unable to login with wrong username', function () {
-		loginPage.tryLogin(this.wrongUsername, this.correctPassword);
-		cy.url().should('eq', `${Cypress.env('fe_base_url') + loginPage.getRoute()}`);
-		cy.contains(
-			loginPage.getFailedLoginBanner(),
-			loginPage.getTexts().wrongUsernameText
-		).should('exist');
-	});
+    it('User should be unable to login with wrong username', function () {
+        loginPage.tryLogin(this.wrongUsername, this.correctPassword);
+        cy.url().should('eq', `${Cypress.env('fe_base_url') + loginPage.getRoute()}`);
+        cy.contains(
+            loginPage.getFailedLoginBanner(),
+            loginPage.getTexts().wrongUsernameText
+        ).should('exist');
+    });
 
-	it('User should be unable to login with wrong password', function () {
-		loginPage.tryLogin(this.correctUsername, this.wrongPassword);
-		cy.url().should('eq', `${Cypress.env('fe_base_url') + loginPage.getRoute()}`);
-		cy.contains(
-			loginPage.getFailedLoginBanner(),
-			loginPage.getTexts().wrongPasswordText
-		).should('exist');
-	});
-	////
+    it('User should be unable to login with wrong password', function () {
+        loginPage.tryLogin(this.correctUsername, this.wrongPassword);
+        cy.url().should('eq', `${Cypress.env('fe_base_url') + loginPage.getRoute()}`);
+        cy.contains(
+            loginPage.getFailedLoginBanner(),
+            loginPage.getTexts().wrongPasswordText
+        ).should('exist');
+    });
 });
